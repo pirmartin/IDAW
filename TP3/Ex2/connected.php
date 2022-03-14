a042fd23321b2fa9690a7e43f598d485d9a7b5c8
pirmartin <pierre.martin@etu.imt-nord-europe.fr>
2022-03-14 17:06:09 +0100

    17:05 14/03

--- TP3/Ex2/connected.php
+++ TP3/Ex2/connected.php
@@ -1,58 +1,20 @@
-<!-- <?php
-    echo "Le login est ".$_GET['login'];
-?>
-<br>
 <?php
-    echo "Le mot de passe est ".$_GET['password'];
-?> -->
-
-<!-- 
-<?php
-// on simule une base de données
-$users = array(
-// login => password
-    'riri' => 'fifi',
-    'yoda' => 'maitrejedi' );
-$login = "anonymous";
-$errorText = "";
-$successfullyLogged = false;
-
-if(isset($_GET['Login']) && isset($_GET['Password'])) {
-    $tryLogin=$_GET['Login'];
-    $tryPwd=$_GET['Password'];
-    // si login existe et password correspond
-    if(array_key_exists($tryLogin, $users) && $users[$tryLogin] == $tryPwd) {
-        $successfullyLogged = true;
-        $login = $tryLogin;
-    } 
-    else
-        $errorText = "Erreur de login/password";
-} 
-else
-    $errorText = "Merci d'utiliser le formulaire de login";
-
-if(!$successfullyLogged) {
-    echo $errorText;
-} 
-else {
-    echo "<h1> Bienvenue ".$login." </h1>";
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$conn = mysqli_connect($servername, $username, $password, 'tp3_idaw');
+if(!$conn){
+    die('Erreur : ' .mysqli_connect_error());
 }
-?> -->
-
-<?php
-// on simule une base de données
-$users = array(
-// login => password
-    'riri' => 'fifi',
-    'yoda' => 'maitrejedi' );
-$login = "anonymous";
-$errorText = "";
-$successfullyLogged = false;
-
+echo 'Connexion réussie <br>';
+$result = $conn->query("SELECT * FROM user");
+$users = array();
+while ($row = $result->fetch_assoc()) {
+    array_push($users, $row['Login'] => $row['Password']);
+}    
 if(isset($_POST['Login']) && isset($_POST['Password'])) {
     $tryLogin=$_POST['Login'];
     $tryPwd=$_POST['Password'];
-    // si login existe et password correspond
     if(array_key_exists($tryLogin, $users) && $users[$tryLogin] == $tryPwd) {
         $successfullyLogged = true;
         $login = $tryLogin;
@@ -62,31 +24,16 @@ if(isset($_POST['Login']) && isset($_POST['Password'])) {
 } 
 else
     $errorText = "Merci d'utiliser le formulaire de login";
-
 if(!$successfullyLogged) {
     echo $errorText;
 } 
 else {
     echo "<h1> Bienvenue ".$login." </h1>";
-    session_start();
-    $_SESSION["Login"] = $login;
-    echo "Login : ".$_SESSION["Login"]."<br>";
 }
-?>
-<!-- 
-<?php
-    echo "Le login est ".$_POST['login'];
-    ?>
-    <br>
-<?php
-    echo "Le mot de passe est ".$_POST['password'];
-?> -->
+?>   
 
-<?php 
-echo "<nav class=\"menu\">
-        <ul>
-            <li class=\"t1\"><a class=<\"t1\" href=\"index.php\">Vers l'index</a></li>
-        </ul>
-    </nav>";
-?>        
-        
+<?php
+$stack = array("orange" => "bleu", "banana"=>"vert");
+array_push($stack, "apple", "raspberry");
+print_r($stack);
+?>     
