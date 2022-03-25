fbdd60155888a4621fda5b7e1a5e5c6b72d6fce7
pirmartin <pierre.martin@etu.imt-nord-europe.fr>
2022-03-25 10:15:16 +0100

    feg

--- TP4/ex2/deleteUser.php
+++ TP4/ex2/deleteUser.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
     $servername = 'localhost';
                 $username = 'root';
                 $password = '';
@@ -21,5 +21,37 @@
         else{
             echo "Error request";
         }
+    }
+?> -->
+
+<?php
+    $servername = 'localhost';
+    $username = 'root';
+    $password = '';
+    $database = 'tp4'; 
+    //On établit la connexion
+    $conn = mysqli_connect($servername, $username, $password, $database);
+
+
+    if(isset($_POST['nom'])){
+        $nom = $_POST['nom'];
+        $prenom = $_POST['prenom'];
+        $date = $_POST['date'];
+        $cours = $_POST['cours'];
+        $remarques = $_POST['remarques'];
+
+        $query = "DELETE FROM tp4 WHERE (Nom='$nom' AND Prenom='$prenom' AND Date_de_naissance='$date')";
+        $result = mysqli_query($conn, $query);
+
+        $res['req']=$query;
+        $res['status']='Erreur requête';
+        if($result)
+            $res['status']='requête OK';
+        print_r($res);
+        echo json_encode($result);
+        
+        /* else{
+            echo 'Erreur requête';
+        } */
     }
 ?>
