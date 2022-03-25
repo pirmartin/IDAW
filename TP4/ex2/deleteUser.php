8466623982e4bddefa1ffb9504ce29f42035810c
pirmartin <pierre.martin@etu.imt-nord-europe.fr>
2022-03-25 09:53:17 +0100

    ead

--- TP4/ex2/deleteUser.php
+++ TP4/ex2/deleteUser.php
@@ -10,11 +10,11 @@
         //stockage des valeurs de POST dans des variables
         $nom = $_POST['nom'];
         $prenom = $_POST['prenom'];
-        $naissance = $_POST['date'];
-        $aime = $_POST['opinion'];
-        $remarques = $_POST['remarques'];
+        //$naissance = $_POST['date'];
+        //$aime = $_POST['opinion'];
+        //$remarques = $_POST['remarques'];
         // requête + incorporation dans la base de données
-        $res = $conn->query("INSERT INTO tp4 (Nom, Prenom, Date_de_naissance, Aime_le_cours, Remarques) VALUES ('$nom','$prenom','$naissance','$aime','$remarques')");
+        $res = $conn->query("DELETE FROM tp4 VALUES (Nom = '$nom' AND Prenom='$prenom'");
         if($res){
             echo json_encode($res);
         }
